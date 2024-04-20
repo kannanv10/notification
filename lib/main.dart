87d33f5513eeb8782b31b94f0e35deb1ad048109
kannanv10 <kannan2003pcwr@gmail.com>
2024-04-20 08:38:16 +0530

    Project notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:notificationflutter/Pages/login/LoginScreen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -8,23 +9,16 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FirebaseMessaging.instance.subscribeToTopic("topic");
+  await FirebaseMessaging.instance.subscribeToTopic("sample");
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print("fcm token: $fcmToken");
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
